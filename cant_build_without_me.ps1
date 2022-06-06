dde9b0412e3be6660529ede10c39b702e4b3c8c7
0x616c <mial.burg@gmail.com>
2022-06-06 15:28:17 +0200

    changes finally

---
+++ cant_build_without_me.ps1
@@ -0,0 +1,4 @@
+git clone "https://github.com/allogic/glad"
+git clone "https://github.com/allogic/glfw"
+git clone "https://github.com/allogic/cglm"
+git clone "https://github.com/allogic/openal"
---
+++ cant_build_without_me.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone "https://github.com/allogic/glad"
+git clone "https://github.com/allogic/glfw"
+git clone "https://github.com/allogic/cglm"
+git clone "https://github.com/allogic/openal"
